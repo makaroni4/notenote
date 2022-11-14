6a2e09782a65f01170dc76d313da5fe1f634be94
Anatoli Makarevich <anatoli@blinkist.com>
2022-11-14 16:44:48 +0100

    Add spec for opening today's note in VS Code

--- spec/lib/note/cli/init_spec.rb
+++ spec/lib/note/cli/init_spec.rb
@@ -6,9 +6,10 @@ describe Note::CLI do
     subject :run do
       clone_config_file!
 
-      described_class.new.run(["init", notes_folder_name])
+      cli.run(["init", notes_folder_name])
     end
 
+    let(:cli) { described_class.new }
     let(:notes_folder_name) { "my_notes" }
     let(:config_file) { File.join(Dir.home, described_class::CONFIG_FILE_NAME) }
     let(:config) { JSON.parse(File.read(config_file)) }
@@ -16,7 +17,7 @@ describe Note::CLI do
     let(:today_note_file) { File.join(notes_folder, Date.today.strftime("%d-%m-%Y"), "today.md") }
 
     before do
-      allow_any_instance_of(described_class).to receive(:system).with("code #{today_note_file}")
+      allow(cli).to receive(:system).with("code #{today_note_file}")
     end
 
     it "creates a .notenote config file in the home directory" do
@@ -46,6 +47,16 @@ describe Note::CLI do
 
     it "creates a default note in today's folder" do
       FakeFS.with_fresh do
+        run
+
+        expect(File).to exist(today_note_file)
+      end
+    end
+
+    it "opens today's note in VS Code" do
+      FakeFS.with_fresh do
+        expect(cli).to receive(:system).with("code #{today_note_file}")
+
         run
 
         expect(File).to exist(today_note_file)
