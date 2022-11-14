5327b1562eb2928b47b993363822d130a86e27d4
Anatoli Makarevich <anatoli@blinkist.com>
2022-11-14 17:12:24 +0100

    Add spec for note command

--- spec/lib/note/cli/default_spec.rb
+++ spec/lib/note/cli/default_spec.rb
@@ -3,7 +3,7 @@ require_relative "../../../../lib/note/cli"
 require_relative "./fake_fs_context"
 
 describe Note::CLI do
-  describe "note on %NOTE% %NAME%" do
+  describe "note" do
     include_context "fake_fs"
 
     subject :run do
@@ -11,29 +11,29 @@ describe Note::CLI do
 
       cli.run(["init", notes_folder_name])
 
-      cli.run(["on", "transformer", "neural", "networks"])
+      cli.run(["on"])
     end
 
     let(:cli) { described_class.new }
     let(:notes_folder_name) { "my_notes" }
     let(:notes_folder) { File.join(Dir.pwd, notes_folder_name) }
-    let(:note_file) { File.join(notes_folder, Date.today.strftime("%d-%m-%Y"), "transformer_neural_networks.md") }
+    let(:today_note_file) { File.join(notes_folder, Date.today.strftime("%d-%m-%Y"), "today.md") }
 
     before do
       allow(cli).to receive(:system)
     end
 
-    it "creates a note file" do
+    it "creates today's note file" do
       FakeFS.with_fresh do
         run
 
-        expect(File).to exist(note_file)
+        expect(File).to exist(today_note_file)
       end
     end
 
-    it "opens a note in VS Code" do
+    it "opens today's note in VS Code" do
       FakeFS.with_fresh do
-        expect(cli).to receive(:system).with("code #{note_file}")
+        expect(cli).to receive(:system).with("code #{today_note_file}")
 
         run
       end
